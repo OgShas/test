f89748751ed659b29b6e127d3230c40288523876
OgShas <shasog@gmail.com>
2025-10-27 10:40:37 +0200

    fix code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(?UserInterface $user): Response
     {
-        $user = $this->getUser();
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'user' => $user,
